8738e5d3f6faa156e2f82efba521d77d3d4aee8c
David Aguilar <davvid@gmail.com>
2019-01-26 22:58:43 -0800

    tests: add unit tests

---
+++ tests/config.rs
@@ -0,0 +1,70 @@
+extern crate garden;
+
+
+fn from_yaml_string(string: &String) -> garden::model::Configuration {
+    let mut config: garden::model::Configuration =
+        std::default::Default::default();
+
+    let file_format = garden::config::FileFormat::YAML;
+    garden::config::parse(string, file_format, false, &mut config);
+
+    return config;
+}
+
+
+/// Test core garden settings
+#[test]
+fn garden_core() {
+    let string = r#"
+garden:
+    root: /tmp
+    environment_variables: false
+    "#.to_string();
+
+    let config = from_yaml_string(&string);
+    assert_eq!(config.root_path, std::path::PathBuf::from("/tmp"));
+    assert_eq!(config.environment_variables, false);
+}
+
+/// Test variables
+#[test]
+fn variables() {
+    let string = r#"
+variables:
+    foo: foo_value
+    bar: ${foo}
+    "#.to_string();
+
+    let config = from_yaml_string(&string);
+    assert_eq!(config.variables.len(), 2);
+
+    assert_eq!(config.variables[0].name, "foo");
+    assert_eq!(config.variables[0].var.expr, "foo_value");
+    assert_eq!(config.variables[0].var.value, None);
+
+    assert_eq!(config.variables[1].name, "bar");
+    assert_eq!(config.variables[1].var.expr, "${foo}");
+    assert_eq!(config.variables[1].var.value, None);
+}
+
+/// Test commands
+#[test]
+fn commands() {
+    let string = r#"
+commands:
+    test_cmd: echo cmd
+    test_cmd_vec:
+        - echo first
+        - echo second
+    "#.to_string();
+
+    let config = from_yaml_string(&string);
+    assert_eq!(config.commands.len(), 2);
+
+    assert_eq!(config.commands[0].name, "test_cmd");
+    assert_eq!(config.commands[0].values[0].expr, "echo cmd");
+
+    assert_eq!(config.commands[1].name, "test_cmd_vec");
+    assert_eq!(config.commands[1].values[0].expr, "echo first");
+    assert_eq!(config.commands[1].values[1].expr, "echo second");
+}
