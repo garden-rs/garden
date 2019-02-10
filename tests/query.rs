af30f33780409dfce3b43275d678a9f5d9e8565e
David Aguilar <davvid@gmail.com>
2019-02-09 23:33:02 -0800

    tests: add query unit tests

---
+++ tests/query.rs
@@ -0,0 +1,23 @@
+extern crate garden;
+
+mod common;
+
+#[test]
+fn default_expression_finds_garden() {
+    let config = common::garden_config();
+    let result = garden::query::resolve_trees(&config, &"cola".to_string());
+    assert_eq!(result.len(), 2);
+    assert_eq!(result[0].garden, Some(0));
+    assert_eq!(result[0].tree, 0);
+    assert_eq!(result[1].garden, Some(0));
+    assert_eq!(result[1].tree, 1);
+}
+
+#[test]
+fn tree_expression_wildcard() {
+    let config = common::garden_config();
+    let result = garden::query::resolve_trees(&config, &"@c*".to_string());
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0].garden, None);
+    assert_eq!(result[0].tree, 1);
+}
