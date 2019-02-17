e08bc64e3b47994ee73af8f8144afb99298a2e52
David Aguilar <davvid@gmail.com>
2019-02-16 22:55:48 -0800

    syntax: alphabetize and add is_exec()

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,6 +1,12 @@
-/// Return true if `string` is a `@tree` expression.
-pub fn is_tree(string: &String) -> bool {
-    string.starts_with("@")
+/// Return true if `string` is an `$ exec` expression.
+pub fn is_exec(string: &String) -> bool {
+    string.starts_with("$ ")
+}
+
+
+/// Return true if `string` is a `:garden` expression.
+pub fn is_garden(string: &String) -> bool {
+    string.starts_with(":")
 }
 
 
@@ -10,7 +16,24 @@ pub fn is_group(string: &String) -> bool {
 }
 
 
-/// Return true if `string` is a `:garden` expression.
-pub fn is_garden(string: &String) -> bool {
-    string.starts_with(":")
+/// Return true if `string` is a `@tree` expression.
+pub fn is_tree(string: &String) -> bool {
+    string.starts_with("@")
+}
+
+
+/// Trim garden, group, and tree prefixes
+pub fn trim(string: &String) -> String {
+    let mut value = string.to_string();
+    value.remove(0);
+    value
+}
+
+
+/// Trim the prefix from an exec expression
+pub fn trim_exec(string: &String) -> String {
+    let mut cmd = string.to_string();
+    cmd.remove(0);
+    cmd.remove(0);
+    cmd
 }
