6cf2813755f532fac6583151759f319405b188c0
David Aguilar <davvid@gmail.com>
2019-02-22 21:44:56 -0800

    command: style tweaks

--- src/command.rs
+++ src/command.rs
@@ -2,27 +2,28 @@ extern crate subprocess;
 
 
 pub fn run(cmd: &Vec<std::path::PathBuf>) -> i32 {
-    let mut result: i32 = 1;
+    let mut exit_status: i32 = 1;
 
     if let Ok(mut p) = subprocess::Popen::create(
             cmd, subprocess::PopenConfig::default()) {
 
-        if let Ok(exit_status) = p.wait() {
-            match exit_status {
+        if let Ok(result) = p.wait() {
+            match result {
                 subprocess::ExitStatus::Exited(status) => {
-                    result = status as i32;
+                    exit_status = status as i32;
                 }
                 subprocess::ExitStatus::Signaled(status) => {
-                    result = status as i32;
+                    exit_status = status as i32;
                 }
                 subprocess::ExitStatus::Other(status) => {
-                    result = status;
+                    exit_status = status;
                 }
                 _ => (),
             }
         }
     }
-    return result;
+
+    exit_status
 }
 
 
@@ -35,5 +36,5 @@ pub fn status(result: subprocess::Result<subprocess::ExitStatus>) -> i32 {
         }
     }
 
-    return exit_status;
+    exit_status
 }
