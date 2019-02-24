f8f548f06e6be49bbadb275e87c1ad3ab817e9f8
David Aguilar <davvid@gmail.com>
2019-02-23 17:53:48 -0800

    syntax: prefer &str references

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,29 +1,29 @@
 /// Return true if `string` is an `$ exec` expression.
-pub fn is_exec(string: &String) -> bool {
+pub fn is_exec(string: &str) -> bool {
     string.starts_with("$ ")
 }
 
 
 /// Return true if `string` is a `:garden` expression.
-pub fn is_garden(string: &String) -> bool {
+pub fn is_garden(string: &str) -> bool {
     string.starts_with(":")
 }
 
 
 /// Return true if `string` is a `%group` expression.
-pub fn is_group(string: &String) -> bool {
+pub fn is_group(string: &str) -> bool {
     string.starts_with("%")
 }
 
 
 /// Return true if `string` is a `@tree` expression.
-pub fn is_tree(string: &String) -> bool {
+pub fn is_tree(string: &str) -> bool {
     string.starts_with("@")
 }
 
 
 /// Trim garden, group, and tree prefixes
-pub fn trim(string: &String) -> String {
+pub fn trim(string: &str) -> String {
     let mut value = string.to_string();
     value.remove(0);
     value
@@ -31,7 +31,7 @@ pub fn trim(string: &String) -> String {
 
 
 /// Trim the prefix from an exec expression
-pub fn trim_exec(string: &String) -> String {
+pub fn trim_exec(string: &str) -> String {
     let mut cmd = string.to_string();
     cmd.remove(0);
     cmd.remove(0);
