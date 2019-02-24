7c1404dd0249766494b305035a6f1c89af1aa0df
David Aguilar <davvid@gmail.com>
2019-02-24 01:00:03 -0800

    cmd: use status() to implement run()

--- src/cmd.rs
+++ src/cmd.rs
@@ -6,21 +6,7 @@ pub fn run(cmd: &Vec<std::path::PathBuf>) -> i32 {
 
     if let Ok(mut p) = subprocess::Popen::create(
             cmd, subprocess::PopenConfig::default()) {
-
-        if let Ok(result) = p.wait() {
-            match result {
-                subprocess::ExitStatus::Exited(status) => {
-                    exit_status = status as i32;
-                }
-                subprocess::ExitStatus::Signaled(status) => {
-                    exit_status = status as i32;
-                }
-                subprocess::ExitStatus::Other(status) => {
-                    exit_status = status;
-                }
-                _ => (),
-            }
-        }
+        exit_status = status(p.wait());
     }
 
     exit_status
@@ -31,8 +17,17 @@ pub fn status(result: subprocess::Result<subprocess::ExitStatus>) -> i32 {
     let mut exit_status: i32 = 1;
 
     if let Ok(status_result) = result {
-        if let subprocess::ExitStatus::Exited(status) = status_result {
-            exit_status = status as i32;
+        match status_result {
+            subprocess::ExitStatus::Exited(status) => {
+                exit_status = status as i32;
+            }
+            subprocess::ExitStatus::Signaled(status) => {
+                exit_status = status as i32;
+            }
+            subprocess::ExitStatus::Other(status) => {
+                exit_status = status;
+            }
+            _ => (),
         }
     }
 
