64d638ae697e641f2994ffa50c1bfa87a7d84f79
David Aguilar <davvid@gmail.com>
2019-04-02 02:03:37 -0700

    init: implement basic a "garden init"

--- src/cmds/init.rs
+++ src/cmds/init.rs
@@ -1,3 +1,8 @@
+extern crate yaml_rust;
+
+use self::yaml_rust::yaml::Yaml;
+use self::yaml_rust::yaml::Hash as YamlHash;
+
 use ::cmd;
 use ::config;
 use ::model;
@@ -28,7 +33,7 @@ pub fn main(options: &mut model::CommandOptions) -> i32 {
     let mut init_options = InitOptions::default();
     {
         let mut ap = argparse::ArgumentParser::new();
-        ap.set_description("garden init - initialize a new garden.yaml");
+        ap.set_description("garden init - create an empty garden.yaml");
 
         ap.refer(&mut init_options.global)
             .add_option(&["--global"], argparse::StoreTrue,
@@ -41,7 +46,8 @@ pub fn main(options: &mut model::CommandOptions) -> i32 {
 
         ap.refer(&mut init_options.root)
             .add_option(&["-r", "--root"], argparse::Store,
-                        "specify the garden root (default: ${GARDEN_CONFIG_DIR}");
+                        "specify the garden root
+                        (default: ${GARDEN_CONFIG_DIR}");
 
         ap.refer(&mut init_options.filename)
             .add_argument("filename", argparse::Store,
@@ -85,6 +91,62 @@ fn init(
         errmsg!("{:?} already exists, use \"--force\" to overwrite",
                 config_path.to_string_lossy());
         return cmd::ExitCode::FileExists.into();
+    }
+
+    // Create parent directories as needed
+    let parent = config_path.parent().as_ref().unwrap().to_path_buf();
+    if !parent.exists() {
+        if let Err(err) = std::fs::create_dir_all(&parent) {
+            errmsg!("unable to create {:?}: {}", parent, err);
+            return cmd::ExitCode::IOError.into();
+        }
+    }
+
+    // Does the config file already exist?
+    let exists = config_path.exists();
+
+    // Read or create a new document
+    let mut doc;
+    if exists {
+        doc = config::reader::read_yaml(&config_path);
+    } else {
+        doc = config::reader::empty_doc();
+    }
+
+    // Mutable scope
+    {
+        if let Yaml::Hash(ref mut doc_hash) = doc {
+            let garden_key = Yaml::String("garden".to_string());
+            let garden: &mut YamlHash = match doc_hash.get_mut(&garden_key) {
+                Some(Yaml::Hash(ref mut hash)) => {
+                    hash
+                },
+                _ => {
+                    errmsg!("invalid configuration: 'garden' is not a hash");
+                    return cmd::ExitCode::Config.into();
+                }
+            };
+
+            let root_key = Yaml::String("root".to_string());
+            garden.remove(&root_key);
+            garden.insert(root_key,
+                          Yaml::String(init_options.root.to_string()));
+        }
+    }
+
+    if !config::writer::write_yaml(&doc, &config_path) {
+        errmsg!("unable to write configuration: {:?}", config_path);
+        return cmd::ExitCode::IOError.into();
+    }
+
+    if !options.quiet {
+        if exists {
+            eprintln!("Reinitialized Garden configuration in {:?}",
+                      config_path);
+        } else {
+            eprintln!("Initialized empty Garden configuration in {:?}",
+                      config_path);
+        }
     }
 
     cmd::ExitCode::Success.into()
