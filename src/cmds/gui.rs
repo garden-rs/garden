c2bf4595ae18ce85f14dffd532eaa2c0ceb6ed0c
David Aguilar <davvid@gmail.com>
2025-01-20 19:58:03 -0800

    gui: use a modal window for command details and improve the tree table

--- src/cmds/gui.rs
+++ src/cmds/gui.rs
@@ -5,43 +5,118 @@ use egui_extras::{Column, TableBuilder};
 use crate::cmds::cmd::CustomOptions;
 use crate::{cmd, errors, model, path, query, syntax};
 
+#[derive(Clone)]
+enum ModalWindow {
+    None,
+    Command(String, Vec<model::Variable>),
+}
+
 struct GardenApp<'a> {
-    query: String,
     app_context: &'a model::ApplicationContext,
+    initialized: bool,
+    modal_window: ModalWindow,
+    modal_window_open: bool,
+    query: String,
 }
 
 impl GardenApp<'_> {
     /// Display details about a command when right-clicked.
-    fn command_button_details(&self, ui: &mut egui::Ui, command_vec: &Vec<model::Variable>) {
+    fn command_details(
+        &mut self,
+        egui_ctx: &egui::Context,
+        command_name: &str,
+        command_vec: &Vec<model::Variable>,
+    ) {
+        let size = egui_ctx.input(|i: &egui::InputState| i.screen_rect());
         let mut value = String::new();
         for cmd in command_vec {
             value.push_str(cmd.get_expr());
             value.push('\n');
         }
+        // Open a modal window with the contents of the command.
         let mut text = value.as_str();
-        ui.add(egui::TextEdit::multiline(&mut text).code_editor().desired_width(f32::INFINITY));
+        egui::Window::new(command_name)
+            .open(&mut self.modal_window_open)
+            .default_width(size.width())
+            .collapsible(false)
+            .resizable(true)
+            .movable(true)
+            .show(egui_ctx, |ui| {
+                ui.vertical(|ui| {
+                    ui.add(
+                        egui::TextEdit::multiline(&mut text)
+                            .code_editor()
+                            .desired_width(f32::INFINITY),
+                    );
+                    ui.with_layout(
+                        egui::Layout::default().with_cross_align(egui::Align::RIGHT),
+                        |ui| {
+                            if ui.button("Close").clicked() {
+                                self.modal_window = ModalWindow::None;
+                            }
+                        },
+                    );
+                });
+            });
     }
 }
 
 impl eframe::App for GardenApp<'_> {
+    /// Display and update the user interface.
     fn update(&mut self, egui_ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let num_columns = 4;
+        let spacing = 4.0;
         let row_height = 18.0;
+        let focus_shortcut = egui::KeyboardShortcut::new(egui::Modifiers::CTRL, egui::Key::L);
+
+        egui_ctx.set_pixels_per_point(2.0);
+        egui_ctx.style_mut(|style| style.visuals.window_shadow = egui::Shadow::NONE);
+
+        if self.modal_window_open {
+            match self.modal_window.clone() {
+                ModalWindow::None => {
+                    self.modal_window_open = false;
+                }
+                ModalWindow::Command(command_name, command_vec) => {
+                    self.command_details(egui_ctx, &command_name, &command_vec);
+                }
+            }
+        }
 
         egui::CentralPanel::default().show(egui_ctx, |ui| {
-            egui_ctx.set_pixels_per_point(2.0);
+            if self.modal_window_open {
+                ui.disable();
+            }
             ui.vertical(|ui| {
                 ui.horizontal(|ui| {
-                    ui.label("Query: ");
-                    ui.add_sized(
+                    ui.label("Query");
+                    ui.add_space(spacing);
+                    let query_response = ui.add_sized(
                         ui.available_size(),
                         egui::TextEdit::singleline(&mut self.query)
                             .hint_text("Tree query for the gardens, groups or trees to execute commands within")
                     );
+                    if !self.initialized {
+                        self.initialized = true;
+                        ui.memory_mut(|memory| {
+                            memory.request_focus(query_response.id);
+                        });
+                    }
+                    if egui_ctx.input_mut(|input| { input.consume_shortcut(&focus_shortcut) }) {
+                        ui.memory_mut(|memory| {
+                            memory.request_focus(query_response.id);
+                        });
+                    }
                 });
 
                 // Global commands
                 ui.separator();
+                ui.with_layout(
+                    egui::Layout::top_down(egui::Align::Center),
+                    |ui| {
+                        ui.label("Commands");
+                    },
+                );
                 let available_width = ui.available_width();
                 let column_width = available_width / (num_columns as f32);
                 egui::Grid::new("command_grid")
@@ -63,10 +138,11 @@ impl eframe::App for GardenApp<'_> {
                         if button.clicked() {
                             println!("Running: {}", command_name);
                         }
-                        button.context_menu(|ui| {
-                            self.command_button_details(ui, command_vec);
-                        });
-                                               
+                        if button.secondary_clicked() {
+                            self.modal_window = ModalWindow::Command(command_name.clone(), command_vec.clone());
+                            self.modal_window_open = true;
+                        }
+
                         current_column += 1;
                         if current_column % num_columns == 0 {
                             current_column = 0;
@@ -78,34 +154,50 @@ impl eframe::App for GardenApp<'_> {
                 // Query results
                 ui.separator();
                 let config = self.app_context.get_root_config_mut();
-                let contexts = query::resolve_trees(self.app_context, config, None, &self.query);
-                TableBuilder::new(ui)
-                    .striped(true)
-                    .cell_layout(egui::Layout::left_to_right(egui::Align::LEFT))
-                    .column(Column::auto().at_least(100.0))
-                    .column(Column::remainder()
-                        .at_least(40.0)
-                        .clip(true)
-                        .resizable(true))
-                    .body(|mut body| {
-                        for tree_ctx in &contexts {
-                            body.row(row_height, |mut row| {
-                                row.col(|ui| {
-                                    ui.label(&tree_ctx.tree);
+                let query = if self.query.is_empty() {
+                    "."
+                } else {
+                    self.query.as_str()
+                };
+                let contexts = query::resolve_trees(self.app_context, config, None, query);
+                if !contexts.is_empty() {
+                    ui.with_layout(
+                        egui::Layout::top_down(egui::Align::Center),
+                        |ui| {
+                            ui.label("Tree Query Results");
+                        },
+                    );
+                    ui.separator();
+                    TableBuilder::new(ui)
+                        .striped(true)
+                        .cell_layout(egui::Layout::left_to_right(egui::Align::LEFT))
+                        .column(Column::auto().at_least(100.0))
+                        .column(Column::remainder()
+                            .at_least(40.0)
+                            .clip(true)
+                            .resizable(true))
+                        .body(|mut body| {
+                            for tree_ctx in &contexts {
+                                body.row(row_height, |mut row| {
+                                    row.col(|ui| {
+                                        ui.with_layout(
+                                            egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                                                ui.add_space(spacing);
+                                                ui.monospace(&tree_ctx.tree);
+                                            },
+                                        );
+                                    });
+                                    let tree = config.get_tree(&tree_ctx.tree);
+                                    if let Some(Ok(path)) = tree.map(|tree| tree.path_as_ref()) {
+                                        row.col(|ui| {
+                                            ui.monospace(path);
+                                        });
+                                    }
                                 });
-                                let tree = config.get_tree(&tree_ctx.tree);
-                                if let Some(Ok(path)) = tree.map(|tree| tree.path_as_ref()) {
-                                    row.col(|ui| {
-                                        ui.label(path);
-                                    });
-                                } else {
-                                    row.col(|ui| {
-                                        ui.label("unknown");
-                                    });
-                                }
-                            });
+                            }
                         }
-                    });
+                    );
+                }
             });
         });
     }
@@ -125,6 +217,9 @@ pub fn main(app_context: &model::ApplicationContext, options: &CustomOptions) ->
     let window_title = format!("Garden - {}", basename);
     let app_state = GardenApp {
         app_context,
+        initialized: false,
+        modal_window_open: false,
+        modal_window: ModalWindow::None,
         query: options.query_string(),
     };
     let result = eframe::run_native(
