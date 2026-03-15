b166dbe1a985779a5febe65335bbb4910281f274
David Aguilar <davvid@gmail.com>
2026-03-15 03:20:47 -0700

    garden v2.6.0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-05ac2b4b.js',
+                path_to_root + 'searchindex-f3773749.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
